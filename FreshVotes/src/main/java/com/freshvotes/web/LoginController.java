cf0b34fd719384fa1309684e7a2699069159f655
Gonçalo <Gonalo@192.168.1.75>
2021-01-28 12:25:37 +0000

    Register a registar nos utilizadores na bd (2)

--- FreshVotes/src/main/java/com/freshvotes/web/LoginController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/LoginController.java
@@ -1,14 +1,34 @@
 package com.freshvotes.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.freshvotes.domain.User;
+import com.freshvotes.services.UserService;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
-		return "/login";
+		return "login";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String register(ModelMap model) {
+		model.put("user", new User());
+		return "register";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPost(User user) {
+		userService.save(user);
+		return "redirect:/login";
 	}
 }
